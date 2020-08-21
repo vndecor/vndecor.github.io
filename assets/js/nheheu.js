668c45dc2a177cb3feeec0928393731b5f40a114
minhhoa <daominhhoa93@gmail.com>
2020-08-21 23:52:08 +0700

    upppgf

--- assets/js/nheheu.js
+++ assets/js/nheheu.js
@@ -148,10 +148,7 @@ var renderItem = function(obj, customclass){
     _html += '<div class="'+ customclass +'" style="display: block;">';
         _html += '<div class="product-image">';
             _html += '<a href="'+ getFullUrl('product/?p='+ obj.slug) +'" class="product-img openlink">';
-                _html += '<img src="https://vndecor.github.io/assets/images/empty.png">';
-                _html += '<img src="'+ obj.imgs[0] +'" class="img">';
-                // _html += '<img class="primary" src="'+ obj.imgs[0] +'">';
-                // _html += '<img class="hover" src="'+ obj.imgs[0] +'">';
+                _html += '<img src="'+ obj.imgs[0] +'">';
                 if( obj.liked ) _html += '<div class="product-liked tooltip bs-tooltip-top" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"><i class="icon anm anm-heart"></i> '+ obj.liked +'</div></div>';
                 if( obj.soldout ) _html += '<span class="sold-out"><img src="https://vndecor.github.io/assets/images/soldout2.png" ></span>';
             _html += '</a>';
